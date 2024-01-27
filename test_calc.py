fb81f986b355ddb9b4f1b9fb1ed6858826cd7894
amdiff001 <155019106+amdiff001@users.noreply.github.com>
2024-01-27 20:14:04 +0200

    Update and rename tests.py to test_calc.py

---
+++ test_calc.py
@@ -0,0 +1,12 @@
+import unittest
+from calc import add
+
+class TestAddition(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
